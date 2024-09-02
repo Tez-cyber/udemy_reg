861250da30107de27213eb09df0f0c6670a56679
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-03 00:49:47 +0100

    Handle form submission completed in Js

---
+++ index.js
@@ -0,0 +1,44 @@
+const regForm = document.getElementById("udReg");
+const errMessage = document.getElementById("regError");
+
+
+regForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const { fullName, email, password } = regForm.elements
+
+    errMessage.innerHTML = "";
+
+    if (!fullName.value.trim()) {
+        showErr("Username is required.");
+        return;
+    }
+
+    if (!email.value.trim() || !isValidEmail(email.value)) {
+        showErr("Please enter a valid email address.");
+        return;
+    }
+
+    if (!password.value.trim() || !isStrongPassword(password.value)) {
+        showErr(
+            "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character."
+        );
+        return;
+    }
+
+    alert("Signup successful!");
+    regForm.reset();
+})
+
+const showErr = (message) => {
+    errMessage.innerHTML += `<div class="error">${message}</div>`;
+}
+
+// ==== Check Password
+const isStrongPassword = (password) => {
+    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/.test(password);
+}
+// ==== Check Email
+const isValidEmail = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
